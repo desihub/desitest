f36cd2c186c1b95fda893aee2ae6105b7c39d707
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2026-05-19 09:02:52 -0700

    fix test config for speclite, specprod-db

--- py/desitest/nersc.py
+++ py/desitest/nersc.py
@@ -87,15 +87,17 @@ def update(basedir=None, logdir='.', repos=None, testonly=False):
 
         #- special cases for testing
         pytestcom = "pytest py/"+repo+"/test"
-        if repo == 'specsim':
+        if repo == 'specsim' or repo == 'speclite':
             pytestcom = "pytest "+repo+"/tests"
+        elif repo == 'specprod-db':
+            pytestcom = "pytest py/specprodDB/test"
         elif repo == 'desisim':
             #- use desisim-testdata for faster testing
             pytestcom = ('module load desisim-testdata && '+pytestcom
                                    +' && '+'module unload desisim-testdata')
 
         if not os.path.exists(repodir):
-            repo_results['status'] = 'FAILURE'
+            repo_results['status'] = 'MISSING'
             repo_results['log'] = 'Missing directory {}'.format(repodir)
             repo_results['updated'] = False
         else:
@@ -122,9 +124,9 @@ def update(basedir=None, logdir='.', repos=None, testonly=False):
                 commands = ['svn update data/',] + commands
 
             #- specsim: python code not under py/
-            if repo == 'specsim':
+            if repo == 'specsim' or repo == 'speclite':
                 i = commands.index('python -m compileall -f ./py')
-                commands[i] = 'python -m compileall -f specsim'
+                commands[i] = f'python -m compileall -f {repo}'
 
             #- desisim-testdata & redrock-templates: data only, no tests
             if repo in ['desisim-testdata', 'redrock-templates']:
